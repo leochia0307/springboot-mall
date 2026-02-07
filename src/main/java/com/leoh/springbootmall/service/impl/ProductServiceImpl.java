1026e14d340744c5ce738437aa76bef18ac0aac5
leonhsieh <elsleon1214@gmail.com>
2026-02-07 09:56:17 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總筆數)

--- src/main/java/com/leoh/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/leoh/springbootmall/service/impl/ProductServiceImpl.java
@@ -18,6 +18,11 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
+    public Integer countProduct(ProductQueryParams productQueryParams) {
+        return productDao.countProduct(productQueryParams);
+    }
+
+    @Override
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
         return productDao.getProducts(productQueryParams);
     }
@@ -41,4 +46,6 @@ public class ProductServiceImpl implements ProductService {
     public void deleteProductById(Integer productId) {
         productDao.deleteProductById(productId);
     }
+
+
 }
